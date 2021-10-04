756a1a17300e4453a683d453d6f29ab8190744a2
toast <smol@toast.cyou>
2021-10-04 14:01:46 -0400

    added prometheus setup

--- build-octopus.sh
+++ build-octopus.sh
@@ -6,6 +6,17 @@ podman login -u toasterrepairman
 # grab grafana
 podman pull grafana/grafana
 # start grafana
-podman run -d --name=grafana -p 3000:3000 grafana/grafana
+podman run \
+    -d --name=grafana \
+    -p 3000:3000 \
+    grafana/grafana
+
+# grab prometheus
+podman pull prom/prometheus
+# start prometheus
+docker run \
+    -p 9090:9090 \
+    -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml \
+    prom/prometheus
 
 # [INSTALL]
