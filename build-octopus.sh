392cb681689df8c3e4edaee9243cc9518dbbbc06
toast <smol@toast.cyou>
2021-10-04 15:18:45 -0400

    mounts systems

--- build-octopus.sh
+++ build-octopus.sh
@@ -2,6 +2,7 @@
 # login to docker because it's garbage software
 podman login -u toasterrepairman
 
+
 # [ORCHESTRATE]
 # grab grafana
 podman pull grafana/grafana
@@ -18,5 +19,12 @@ podman run -d --rm -it \
 -p 9090:9090 \
 prom/prometheus
 
+
 # [INSTALL]
 sudo cp resources/services/* /etc/systemd/system/
+
+
+# [STARTUP]
+# begin prometheus systems
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
